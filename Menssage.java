ff5c1315c2d8871c36795aa62ea225475a9a9a82
repeat-nina <106721513+repeat-nina@users.noreply.github.com>
2022-08-12 12:00:24 -0500

    Update Menssage.java

--- Menssage.java
+++ Menssage.java
@@ -3,11 +3,11 @@ import javax.swing.JOptionPane;
 public class Menssage {
         public static void main(String[] args) {
                 MyIcon icon = new MyIcon();
-                JOptionPane.showMessageDialog(null, "No voy a dejar de quererte", "∿ Para ella",
+                JOptionPane.showMessageDialog(null, "Me enamore", "∿ Ella no te ama",
                                 JOptionPane.DEFAULT_OPTION, icon);
-                JOptionPane.showMessageDialog(null, "No voy a dejar de abrazarte", "∿ Para ella",
+                JOptionPane.showMessageDialog(null, "De alguien", "∿ Ella no te ama",
                                 JOptionPane.DEFAULT_OPTION, icon);
-                JOptionPane.showMessageDialog(null, "No voy a dejar de besarte", "∿ Para ella",
+                JOptionPane.showMessageDialog(null, "Que tambien...", "∿ Ella no te ama",
                                 JOptionPane.DEFAULT_OPTION, icon);
                 JOptionPane.showMessageDialog(null, "Se enamoro /(ㄒoㄒ)/~~", "∿ Ella no te ama",
                                 JOptionPane.DEFAULT_OPTION, icon);
