9d95adf7fda87bd3e2865e4c78a5f3a285ab6988
repeat-nina <106721513+repeat-nina@users.noreply.github.com>
2022-08-11 22:22:11 -0500

    Add files via upload

---
+++ Menssage.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Menssage {
+        public static void main(String[] args) {
+                MyIcon icon = new MyIcon();
+                JOptionPane.showMessageDialog(null, "No voy a dejar de quererte", "∿ Para ella",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "No voy a dejar de abrazarte", "∿ Para ella",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "No voy a dejar de besarte", "∿ Para ella",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Se enamoro /(ㄒoㄒ)/~~", "∿ Ella no te ama",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "Y tristemente （︶^︶）", "∿ Ella no te ama",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "No pude... ( ˘︹˘ )", "∿ Ella no te ama",
+                                JOptionPane.DEFAULT_OPTION, icon);
+                JOptionPane.showMessageDialog(null, "ser yo (；′⌒`)", "∿ Ella no te ama",
+                                JOptionPane.DEFAULT_OPTION, icon);
+        }
+}
